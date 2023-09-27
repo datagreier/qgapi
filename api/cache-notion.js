86893b1698ea17f203587a5001fb9feff950e87b
datagreier <58817035+datagreier@users.noreply.github.com>
2023-09-27 12:37:35 +0200

    Update cache-notion.js - New code with Octokit

--- api/cache-notion.js
+++ api/cache-notion.js
@@ -1,17 +1,15 @@
 const fetch = require('node-fetch');
-const fs = require('fs/promises');
-const path = require('path');
+const { Octokit } = require('@octokit/rest');
 
 const NOTION_API_KEY = "secret_L5EkN7Il9rEm9QfPNKRx8Lca5Q6m0sfyvK9yoYMtw9Z";
 const NOTION_DATABASE_ID = "c68a45e247104d2c9099c729477cda69";
-const GITHUB_TOKEN = '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'; // Replace with your GitHub token
-const GITHUB_OWNER = 'datagreier'; // Replace with your GitHub username
-const GITHUB_REPO = 'qgdata'; // Replace with the target GitHub repository
+const GITHUB_TOKEN = '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';
+const GITHUB_OWNER = 'datagreier';
+const GITHUB_REPO = 'qgdata';
 const FILE_PATH = 'dbcache/data.json';
 
 async function fetchNotionData() {
   try {
-    // Initialize Notion API URL and headers
     const notionUrl = `https://api.notion.com/v1/databases/${NOTION_DATABASE_ID}/query`;
     const headers = {
       Authorization: `Bearer ${NOTION_API_KEY}`,
@@ -19,7 +17,6 @@ async function fetchNotionData() {
       'Content-Type': 'application/json',
     };
 
-    // Fetch data from Notion API
     console.log('Fetching data from Notion API...');
     const notionResponse = await fetch(notionUrl, {
       method: 'POST',
@@ -34,33 +31,23 @@ async function fetchNotionData() {
     const data = await notionResponse.json();
     console.log('Data received from Notion:', data);
 
-    // Write the fetched data to a local file
-    const localFilePath = path.join(__dirname, FILE_PATH);
-    await fs.writeFile(localFilePath, JSON.stringify(data, null, 2));
-    console.log(`Data written to ${localFilePath} successfully`);
-
-    // Push the local file to the target GitHub repository
-    await pushDataToGitHub(localFilePath);
+    await pushDataToGitHub(JSON.stringify(data, null, 2));
   } catch (error) {
     console.error('Error occurred:', error);
   }
 }
 
-async function pushDataToGitHub(filePath) {
+async function pushDataToGitHub(fileContents) {
   try {
     const octokit = new Octokit({ auth: GITHUB_TOKEN });
 
-    // Read the file contents
-    const fileContents = await fs.readFile(filePath, 'utf8');
-
-    // Create or update a file in the target repository
     await octokit.rest.repos.createOrUpdateFileContents({
       owner: GITHUB_OWNER,
       repo: GITHUB_REPO,
       path: FILE_PATH,
       message: 'Update data from Notion',
       content: Buffer.from(fileContents).toString('base64'),
-      branch: 'main', // Replace with the target branch name
+      branch: 'main',
     });
 
     console.log(`Data pushed to the ${GITHUB_REPO} repository successfully`);
